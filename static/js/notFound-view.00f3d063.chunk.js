69c0db05dd64d51b9c46fa84a53767449f279ec0
kirillbodnar <91017699+kirillbodnar@users.noreply.github.com>
2022-06-29 23:45:55 +0000

    Deploying to gh-pages from @ kirillbodnar/goit-react-hw-05-movies@bf85eb2f16613fd2ff74419b6212ff2212361007 🚀

---
+++ static/js/notFound-view.00f3d063.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[566],{566:function(e,n,t){t.r(n),t.d(n,{default:function(){return c}});var r=t(184);function c(){return(0,r.jsx)(r.Fragment,{children:(0,r.jsx)("h2",{children:"404 \u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430"})})}}}]);
+//# sourceMappingURL=notFound-view.00f3d063.chunk.js.map
